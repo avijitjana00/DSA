4693ee8b24dabb332ceda1d3839c612098e276cd
avijit <avijitjana@gmail.com>
2026-05-14 12:14:53 +0530

    Insertion sort

---
+++ Searching & Sorting/insertionSort.js
@@ -0,0 +1,18 @@
+const arr = [7, 4, 3, 5, 1, 2];
+
+function insertionSort(arr){
+    const n = arr.length;
+    for(let i = 1; i < n; i++){
+        let curr = arr[i];
+        let prev = i - 1;
+        while(arr[prev] > curr && prev >= 0){
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr;
+    }
+    return arr;
+}
+
+const output = insertionSort(arr);
+console.log(output);
